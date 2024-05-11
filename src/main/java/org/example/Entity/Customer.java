cf3a914c42843c71f5ae46cc7d1485e38c12f1ef
David Lara <dlara7913@gmail.com>
2024-05-10 20:59:27 -0700

    -Added MealPlanStrategy strategy to Customer.java

--- src/main/java/org/example/Entity/Customer.java
+++ src/main/java/org/example/Entity/Customer.java
@@ -1,9 +1,13 @@
 package org.example.Entity;
 
+import org.example.Entity.Strategy.MealPlanStrategy;
+
 public class Customer {
     private String name;
     private Address address;
     private County county;
+
+    private MealPlanStrategy mealPlanStrategy;
 
     public Customer(String name, Address address, County county){
         this.name = name;
@@ -22,5 +26,13 @@ public class Customer {
 
     public County getCounty() {
         return county;
+    }
+
+    public void setMealPlanStrategy(MealPlanStrategy plan){
+        mealPlanStrategy = plan;
+    }
+
+    public MealPlanStrategy getMealPlanStrategy(){
+        return mealPlanStrategy;
     }
 }
