193c3ed3a308fba6530bb7f84a96afc3c886902f
David Lara <dlara7913@gmail.com>
2024-05-15 13:55:16 -0700

    -Modified imports to better reflect from which package they arrived from.

--- src/main/java/org/example/Entity/Mediator/MediatorInterface.java
+++ src/main/java/org/example/Entity/Mediator/MediatorInterface.java
@@ -1,12 +1,12 @@
 package org.example.Entity.Mediator;
 
-import org.example.Driver;
+import org.example.Entity.Driver;
 import org.example.Entity.Customer;
-import org.example.Entity.Resturant;
+import org.example.Entity.Restaurant;
 
 public interface MediatorInterface {
 
-    public void registerResturant(Resturant resturant);
+    public void registerResturant(Restaurant restaurant);
     public void registerDriver(Driver driver);
 
     public void registerCustomer(Customer customer);
